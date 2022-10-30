d2889406c474012bbda96f89f3120c7f59efd5fb
Michal <valarath@gmail.com>
2022-10-30 21:46:39 +0100

    fix for userUpdate in case that user is not fetchable

--- database/src/main/java/cz/honestcity/database/user/UserPostgresMapper.java
+++ database/src/main/java/cz/honestcity/database/user/UserPostgresMapper.java
@@ -6,20 +6,20 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface UserPostgresMapper {
 
-    String TO_USER ="toUser";
+    String TO_USER = "toUser";
 
     @Select("SELECT\n" +
             "  user_id,\n" +
             "  score,\n" +
-            "  username\n" +
+            "  username,\n" +
             "  email\n" +
             "FROM \"user\"\n" +
             "WHERE user_id = #{userId}")
     @Results(id = TO_USER, value = {
-            @Result(property = "id",column = "user_id"),
-            @Result(property = "score",column = "score"),
-            @Result(property = "username",column = "username"),
-            @Result(property = "email",column = "email")
+            @Result(property = "id", column = "user_id"),
+            @Result(property = "username", column = "username"),
+            @Result(property = "score", column = "score"),
+            @Result(property = "email", column = "email")
     })
     User getUser(@Param("userId") String userId);
 
@@ -34,13 +34,13 @@ public interface UserPostgresMapper {
     int getUserScore(@Param("userId") String userId);
 
     @Insert("INSERT INTO \"user\" (user_id, username, score, email) VALUES (#{user.id}, #{user.username}, #{user.score}, #{user.email});")
-    void saveNewUser(@Param("user")User user);
+    void saveNewUser(@Param("user") User user);
 
     @Update("UPDATE \"user\" \n" +
             "SET username = #{user.username}, \n" +
             "email = #{user.email} \n" +
             "WHERE user_id = #{user.id};")
-    void updateUserData(@Param("user")User user);
+    void updateUserData(@Param("user") User user);
 
     @Select("SELECT\n" +
             "  user_id,\n" +
